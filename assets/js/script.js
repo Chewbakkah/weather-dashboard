e3cb1740634c8ead613812939060cb60d04930ae
Chewbakkah <chewbakkah@gmail.com>
2021-12-18 01:58:16 -0600

    js css files started, pseudocode started

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+let navEl = document.querySelector("#nav");
+let citySearchEl = document.querySelector("#city-search");
+let pastSearchEl = document.querySelector("#past-search");
+let currentWeatherEl = document.querySelector("#current-weather");
+let currentMapEl = document.querySelector("#current-map");
+let futureEl = document.querySelector("#future");
+
+//TO Do:
+// Capture search form and turn into coordinates for weather API
+// This may require a new secondary API?
+
+// Store search results in local storage and display on main page as options
+// check search against already stored cities to prevent duplicates
+// Make delete button for removing these clickable cities
+
+// Call info from weather API and store variables
+// current weather should provide the following: city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// UV index needs to be colored based on severity 
+// 6 day forcast needed
+// style info for display on main page
+// push info to main page sections
+// push info with variable classes so that css file can style based on conditions
+
+// style elements in css file
+// style conditional classes based on weather type
+
+// BONUS STUFF:
+// add date and time to app
+// add background weather images
+// add dark and light modes
