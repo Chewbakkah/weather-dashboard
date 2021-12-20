351e2a6864273d0fa084db7b7d060bc2cc5e5e65
Chewbakkah <chewbakkah@gmail.com>
2021-12-20 02:10:07 -0600

    stored current and future weather info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+let apiKey = "579b64ec752a4651bfdc49c3c591949e";
 let navEl = document.querySelector("#nav");
 let citySearchEl = document.querySelector("#city-search");
 let stateSearchEl = document.querySelector("#state");
@@ -5,23 +6,105 @@ let pastSearchEl = document.querySelector("#past-search");
 let currentWeatherEl = document.querySelector("#current-weather");
 let currentMapEl = document.querySelector("#current-map");
 let futureEl = document.querySelector("#future");
+let goBtnEl = document.querySelector("#goBtn");
+let cityErrorEl = document.querySelector("#city-error");
+let citySearch;
+let stateSearch;
+let lat;
+let lon;
+let currentTemp;
+let currentFeel;
+let currentWind;
+let currentHumidity;
+let currentUV;
+//0 = Weather Type | 1 = weather icon | 2 = temp low | 3 = temp high | 4 = wind speed | 5 = humidity
+let dayArray = [[],[],[],[],[],[]];
+let dayArray2 = [];
+let dayArray3 = [];
+let dayArray4 = [];
+let dayArray5 = [];
+let dayArray6 = [];
 
-let getEndpoint = function() {
-    endpoint = `https://api.openweathermap.org/data/2.5/weather?q=Houston,tx,usa&units=imperial&appid=579b64ec752a4651bfdc49c3c591949e`
+// weather API https://openweathermap.org/
+
+
+let captureCity = function(event){
+    event.preventDefault();
+    if (citySearchEl.value == ""){
+        cityErrorEl.classList.remove("hidden");
+    }else{
+        cityErrorEl.classList.add("hidden");
+        citySearch = citySearchEl.value;
+        stateSearch = stateSearchEl.value;
+        localStorage.setItem("lastCity", citySearch);
+        localStorage.setItem("lastState", stateSearch);
+        getEndpoint();
+
+    }
 }
-getEndpoint();
+let getEndpoint2 = function() {
+    endpoint = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+
 fetch(endpoint)
     .then(function (response){
         return response.json();
     })
     .then(function (jsonData){
         console.log(jsonData);
+        currentUV = jsonData.current.uvi;
+        for(i = 1; i < 7; i++){
+                let weatherTemp = jsonData.daily[i+1].weather[0].main;
+                dayArray[i-1][0] = weatherTemp;
+                let weatherIcon = jsonData.daily[i+1].weather[0].icon;
+                dayArray[i-1][1] = weatherIcon;
+                let tempLow = jsonData.daily[i+1].temp.min;
+                dayArray[i-1][2] = tempLow;
+                let tempHigh = jsonData.daily[i+1].temp.max;
+                dayArray[i-1][3] = tempHigh;
+                let windSpeed = jsonData.daily[i+1].wind_speed;
+                dayArray[i-1][4] = windSpeed;
+                let humidity = jsonData.daily[i+1].humidity;
+                dayArray[i-1][5] = humidity;
+            }
+        console.log(dayArray);     
     })
+};
+
+let getEndpoint1 = function() {
+    endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch},${stateSearch},usa&units=imperial&appid=${apiKey}`
+
+fetch(endpoint)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (jsonData){
+        console.log(jsonData);
+        lat = jsonData.coord.lat;
+        lon = jsonData.coord.lon;
+        currentTemp = jsonData.main.temp;
+        currentFeel = jsonData.main.feels_like;
+        currentWind = jsonData.wind.speed;
+        currentHumidity = jsonData.main.humidity;
+        
+    })
+    .then(function(){
+        getEndpoint2();})}
 
 
+    let loadData = function(){
+    if(localStorage.getItem("lastCity") !== null){
+        citySearch = localStorage.getItem("lastCity");
+        citySearchEl.value = citySearch;
+        stateSearch = localStorage.getItem("lastState");
+        stateSearchEl.value = stateSearch;
+        getEndpoint1();
+        
+        
+    }
+}
 
-
-
+loadData();
+goBtnEl.addEventListener("click", captureCity);
 //TO Do:
 // Capture search form and turn into coordinates for weather API
 // This may require a new secondary API?
@@ -45,3 +128,4 @@ fetch(endpoint)
 // add date and time to app
 // add background weather images
 // add dark and light modes
+// local time?
